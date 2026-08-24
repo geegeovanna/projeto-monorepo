49b78d273ba48281ee3e1230b7b68a843e213561
geegeovanna <geovanna.psilva10@gmail.com>
2026-08-24 20:47:06 -0300

    wip: MVC inicial

---
+++ backend/src/server.ts
@@ -0,0 +1,71 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { sequelize } from './config/database';
+import { User } from './models/User';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rota de Health Check
+app.get('/api/health', (req: Request, res: Response) => {
+    res.status(200).json({
+        status: "OK",
+        mensagem: "Servidor Backend rodando com sucesso.",
+        timeStamp: new Date().toISOString()
+    });
+});
+
+// Cadastrar os usuarios
+app.post('/api/users', async (req: Request, res: Response) => {
+    try {
+        const { nome, email, senha_hash } = req.body;
+
+        if (!nome || !email || !senha_hash) {
+            return res.status(400).json({ erro: 'nome, email e senha_hash são obrigatórios.'})
+        }
+
+        const novoUsuario = await User.create({nome, email, senha_hash});
+
+        return res.status(201).json(novoUsuario);
+    } catch (error: any) {
+        return res.status(500).json({erro: 'Erro ao cadastrar usuario.', detalhe: error.message})
+    }
+})
+
+// Listar todos os usuarios
+app.get('/api/users', async (req: Request, res: Response) => {
+    try {
+        const usuarios = await User.findAll({
+            attributes: ['id', 'nome', 'email', 'createdAt']
+        });
+
+        return res.status(200).json(usuarios);
+        
+    } catch (error: any) {
+        return res.status(500).json({erro: 'Erro ao listar usuarios.', detalhe: error.message})
+    }
+})
+
+async function main() {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexão com o PostgreSQL no Supabase realizada com sucesso.');
+
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+            console.log(`Health Check disponível em: http://localhost:${PORT}/api/health`);
+        });
+
+    } catch(error) {
+        console.log('Erro ao conectar como banco de dados', error);
+    }
+}
+
+main();
